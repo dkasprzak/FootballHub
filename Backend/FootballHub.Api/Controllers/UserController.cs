b76e5bd5338f41df91c8b4d40b7a3cff2fc0eae3
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-06-03 13:56:37 +0200

    added user controller

---
+++ Backend/FootballHub.Api/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using FootballHub.Application.Logic.User;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballHub.Api.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class UserController : BaseController
+{
+    public UserController(ILogger logger, IMediator mediator) : base(logger, mediator)
+    {
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateUserWithAccount([FromBody] CreateUserWithAccountCommand.Request model)
+    {
+        var createAccountResult = await _mediator.Send(model);
+        return Ok(createAccountResult);
+    }
+    
+}
