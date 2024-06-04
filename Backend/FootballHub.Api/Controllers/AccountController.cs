d624a04a6da131ce44cc49512f7825950175410e
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-06-04 14:07:02 +0200

    Added Current Account Query

---
+++ Backend/FootballHub.Api/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using FootballHub.Application.Logic.Account;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballHub.Api.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class AccountController : BaseController
+{
+    public AccountController(ILogger<AccountController> logger, IMediator mediator) : base(logger, mediator)
+    {
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult> GetCurrentAccount()
+    {
+        var data = await _mediator.Send(new CurrentAccountQuery.Request());
+        return Ok(data);
+    }
+}
---
+++ Backend/FootballHub.Application/Logic/Account/CurrentAccountQuery.cs
@@ -0,0 +1,45 @@
+﻿using EFCoreSecondLevelCacheInterceptor;
+using FluentValidation;
+using FootballHub.Application.Exceptions;
+using FootballHub.Application.Interfaces;
+using FootballHub.Application.Logic.Abstractions;
+using FootballHub.Application.Logic.User;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballHub.Application.Logic.Account;
+
+public static class CurrentAccountQuery
+{
+    public class Request : IRequest<Result>;
+
+    public class Result
+    {
+        public required string Name { get; set; }
+    }
+
+    public class Handler : BaseQueryHandler, IRequestHandler<Request, Result>
+    {
+
+        public Handler(ICurrentAccountProvider currentAccountProvider, 
+            IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+        {
+        }
+
+        public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var account = await _currentAccountProvider.GetAuthenticatedAccount();
+            return new Result()
+            {
+                Name = account.Name
+            };
+        }
+        
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+            }
+        }
+    }
+}
