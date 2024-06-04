976277fe579fad2c6b144b3a0353c1978242cd69
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-06-04 18:17:55 +0200

    changed defaut.vue

--- Frontend/layouts/default.vue
+++ Frontend/layouts/default.vue
@@ -1,6 +1,27 @@
 <template>
-    <VApp>
-      <NuxtPage />
-    </VApp>
-  </template>
-  
+  <v-app id="inspire">
+    <v-app-bar>
+      <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>FootballHub</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main>
+      <div class="pa-4">
+        <NuxtPage />
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { useDisplay } from "vuetify";
+
+  const { mobile } = useDisplay();
+
+  const drawer = ref(false)
+</script>
